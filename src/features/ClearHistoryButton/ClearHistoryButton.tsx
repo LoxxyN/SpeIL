0fabc17cd672e1b87284b26dd86f0774b752c77f
loxxyN <emoyko@yandex.ru>
2026-05-25 02:39:18 +0700

    feat(confirm-delete-modal): Added a modal window with confirm of the action on button click
    style(layout): The page elements are correctly pressed against the edges of the page, regardless of the height of the page

--- src/features/ClearHistoryButton/ClearHistoryButton.tsx
+++ src/features/ClearHistoryButton/ClearHistoryButton.tsx
@@ -1,5 +1,41 @@
+import { AlertDialog, Button } from '@heroui/react'
 import { DeleteButton } from '@shared/ui'
 
 export const ClearHistoryButton = ({ onRemoveReview }: { onRemoveReview: () => void }) => {
-  return <DeleteButton onRemoveReview={onRemoveReview} label="Очистить" hasTrashIcon />
+  return (
+    <AlertDialog>
+      <DeleteButton label="Очистить все" hasTrashIcon />
+      <AlertDialog.Backdrop variant="blur">
+        <AlertDialog.Container size="md">
+          <AlertDialog.Dialog>
+            <AlertDialog.CloseTrigger />
+            <AlertDialog.Header>
+              <AlertDialog.Icon status="danger" />
+              <AlertDialog.Heading>
+                Вы действительно <strong>хотите</strong> очистить историю?
+              </AlertDialog.Heading>
+            </AlertDialog.Header>
+            <AlertDialog.Body>
+              <p>
+                Это удалит историю безвозвратно. После удаления нельзя будет вернуть удаленные
+                ревью, делайте это решение осознанно
+              </p>
+            </AlertDialog.Body>
+            <AlertDialog.Footer>
+              <Button slot="close" variant="tertiary">
+                Отмена
+              </Button>
+              <DeleteButton
+                size="md"
+                slot="close"
+                onClick={onRemoveReview}
+                hasTrashIcon={false}
+                label="Подтвердить"
+              />
+            </AlertDialog.Footer>
+          </AlertDialog.Dialog>
+        </AlertDialog.Container>
+      </AlertDialog.Backdrop>
+    </AlertDialog>
+  )
 }
--- src/features/RemoveReviewButton/RemoveReviewButton.tsx
+++ src/features/RemoveReviewButton/RemoveReviewButton.tsx
@@ -1,5 +1,38 @@
+import { AlertDialog, Button } from '@heroui/react'
 import { DeleteButton } from '@shared/ui'
 
 export const RemoveReviewButton = ({ onRemoveReview }: { onRemoveReview: () => void }) => {
-  return <DeleteButton onRemoveReview={onRemoveReview} label="Удалить" hasTrashIcon />
+  return (
+    <AlertDialog>
+      <DeleteButton label="Удалить" hasTrashIcon />
+      <AlertDialog.Backdrop variant="blur">
+        <AlertDialog.Container size="sm">
+          <AlertDialog.Dialog>
+            <AlertDialog.CloseTrigger />
+            <AlertDialog.Header>
+              <AlertDialog.Icon status="danger" />
+              <AlertDialog.Heading>
+                Вы действительно <strong>хотите</strong> удалить это ревью?
+              </AlertDialog.Heading>
+            </AlertDialog.Header>
+            <AlertDialog.Body>
+              <p>Это удалит ревью безвозвратно, без возможности восстановления</p>
+            </AlertDialog.Body>
+            <AlertDialog.Footer>
+              <Button slot="close" variant="tertiary">
+                Отмена
+              </Button>
+              <DeleteButton
+                size="md"
+                slot="close"
+                onClick={onRemoveReview}
+                hasTrashIcon={false}
+                label="Подтвердить"
+              />
+            </AlertDialog.Footer>
+          </AlertDialog.Dialog>
+        </AlertDialog.Container>
+      </AlertDialog.Backdrop>
+    </AlertDialog>
+  )
 }
