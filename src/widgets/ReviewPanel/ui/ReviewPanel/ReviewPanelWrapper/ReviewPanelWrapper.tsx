c80ad3b0472a33e084b298319a0a3e70cc660a71
loxxyN <emoyko@yandex.ru>
2026-05-27 02:56:29 +0700

    feat(ReviewHistoryCard): Made the transition from the card to the desired page
    refactor(RemoveReviewButton, DeleteButton): Added a restriction on the event surfacing when the button is clicked
    refactor(CodeEditor): Removed the additional class for the theme, replaced it with [data-theme] in css
    refactor(ReviewPanelWrapper): Removed theme initialization from the code
    
    style(ReviewPanelWrapper): The theme value is pulled up from [data-theme], increased the height of the section
    style(CodeEditor): Removed the unnecessary frame
    style(ReviewHistoryCard): Increased the width of the code block in the card

--- src/widgets/ReviewPanel/ui/ReviewPanel/ReviewPanelWrapper/ReviewPanelWrapper.tsx
+++ src/widgets/ReviewPanel/ui/ReviewPanel/ReviewPanelWrapper/ReviewPanelWrapper.tsx
@@ -1,4 +1,3 @@
-import { themeStore } from '@/app/store'
 import { ReviewResult } from '@entities/index'
 import { CodeEditor, CopyCodeButton } from '@features/index'
 import type { TReviewData } from '@shared/types'
@@ -26,7 +25,6 @@ export const ReviewPanelWrapper = ({
   clearEditor,
   getReview,
 }: TReviewPanel) => {
-  const { theme } = themeStore
   const renderActionButtons = () => {
     if (!showActions) {
       return null
@@ -49,8 +47,8 @@ export const ReviewPanelWrapper = ({
   }
 
   return (
-    <section className={`review-panel ${theme === 'dark' ? 'review-panel--dark' : ''} `}>
-      <div className="flex w-2/5 flex-col gap-6">
+    <section className="review-panel">
+      <div className="review-panel__code-editor w-2/5">
         <div className="editor-container">
           {renderCodeEditor()}
           <CopyCodeButton code={code} />
